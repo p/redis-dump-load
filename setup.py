2f11af0ccd625a88ce617abd64fdfd43c7702d13
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-16 04:54:44 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='redis-dump-load',
+    version='0.1.0',
+    description='Dump and load redis databases',
+    author='Oleg Pudeyev',
+    author_email='oleg@bsdpower.com',
+    url='http://github.com/p/redis-dump-load',
+    py_modules=['redisdl'],
+)
