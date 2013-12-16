eac4eb188a44986a0c2c01078fde4e809a2bb269
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-16 05:33:48 -0500

    Update version to have accurate packages on pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='redis-dump-load',
-    version='0.1.0',
+    version='0.1.1',
     description='Dump and load redis databases',
     author='Oleg Pudeyev',
     author_email='oleg@bsdpower.com',
