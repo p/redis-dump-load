d5a1bfbff18cf129550c2c423beb8db9302c0736
Oleg Pudeyev <oleg@bsdpower.com>
2014-01-24 23:53:21 -0500

    Clear redis data store before running tests

--- tests/redisdl_test.py
+++ tests/redisdl_test.py
@@ -4,6 +4,12 @@ import json
 import os.path
 
 class RedisdlTest(unittest.TestCase):
+    def setUp(self):
+        import redis
+        self.r = redis.Redis()
+        for key in self.r.keys('*'):
+            self.r.delete(key)
+
     def test_roundtrip(self):
         path = os.path.join(os.path.dirname(__file__), 'fixtures', 'dump.json')
         with open(path) as f:
