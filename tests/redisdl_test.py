73d9049dea55ddfa32e4cb09f969b6ff083fee2c
Oleg Pudeyev <oleg@bsdpower.com>
2014-01-24 23:53:46 -0500

    Add tests for dumping string and unicode values

--- tests/redisdl_test.py
+++ tests/redisdl_test.py
@@ -21,5 +21,19 @@ class RedisdlTest(unittest.TestCase):
 
         expected = json.loads(dump)
         actual = json.loads(redump)
-        
+
+        self.assertEqual(expected, actual)
+
+    def test_dump_string_value(self):
+        self.r.set('key', 'value')
+        dump = redisdl.dumps()
+        actual = json.loads(dump)
+        expected = {'key': {'type': 'string', 'value': 'value'}}
+        self.assertEqual(expected, actual)
+
+    def test_dump_unicode_value(self):
+        self.r.set('key', u"\u041c\u043e\u0441\u043a\u0432\u0430")
+        dump = redisdl.dumps()
+        actual = json.loads(dump)
+        expected = {'key': {'type': 'string', 'value': u"\u041c\u043e\u0441\u043a\u0432\u0430"}}
         self.assertEqual(expected, actual)
