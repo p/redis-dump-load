a7a0ab2f9467e375778d4bc5e627d79908a85275
Oleg Pudeyev <oleg@bsdpower.com>
2014-01-25 03:43:42 -0500

    Add unicode loading and dumping tests for the program

--- tests/program_test.py
+++ tests/program_test.py
@@ -5,6 +5,8 @@ import json
 import os.path
 from . import util
 
+unicode_dump = {u'akey': {u'type': u'string', u'value': u'\u041c\u043e\u0441\u043a\u0432\u0430'}, u'lvar': {u'type': u'list', u'value': [u'\u041c\u043e\u0441\u043a\u0432\u0430']}, u'svar': {u'type': u'set', u'value': [u'\u041c\u043e\u0441\u043a\u0432\u0430']}, u'zvar': {u'type': u'zset', u'value': [[u'\u041c\u043e\u0441\u043a\u0432\u0430', 1.0]]}, u'hvar': {u'type': u'hash', u'value': {u'hkey': u'\u041c\u043e\u0441\u043a\u0432\u0430'}}}
+
 class ProgramTest(unittest.TestCase):
     def setUp(self):
         import redis
@@ -28,6 +30,15 @@ class ProgramTest(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
+    def test_dump_unicode(self):
+        redisdl.loads(json.dumps(unicode_dump))
+
+        redump = subprocess.check_output([self.program])
+
+        actual = json.loads(redump)
+
+        self.assertEqual(unicode_dump, actual)
+
     def test_load(self):
         path = os.path.join(os.path.dirname(__file__), 'fixtures', 'dump.json')
         with open(path) as f:
@@ -41,3 +52,17 @@ class ProgramTest(unittest.TestCase):
         actual = json.loads(redump)
 
         self.assertEqual(expected, actual)
+
+    def test_load_unicode(self):
+        path = os.path.join(os.path.dirname(__file__), 'fixtures', 'dump-unicode.json')
+        with open(path) as f:
+            dump = f.read()
+
+        subprocess.check_call([self.program, '-l', path])
+
+        redump = redisdl.dumps()
+
+        actual = json.loads(redump)
+
+        self.maxDiff = None
+        self.assertEqual(unicode_dump, actual)
