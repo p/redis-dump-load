fde62ae0e68db6951f74245c2e93e09cfc99164b
Oleg Pudeyev <oleg@bsdpower.com>
2015-11-30 00:43:17 -0500

    Use shell syntax

--- tests/travis/run.sh
+++ tests/travis/run.sh
@@ -5,11 +5,11 @@ set -e
 NOSETESTS=nosetests
 
 if test "$TEST" = slow; then
-  $(NOSETESTS) -a '!yajl2,slow'
+  $NOSETESTS -a '!yajl2,slow'
 else
-  $(NOSETESTS) -a '!yajl2,!slow'
+  $NOSETESTS -a '!yajl2,!slow'
 fi
 
 if test -n "$YAJL2"; then
-  $(NOSETESTS) -a 'yajl2,!slow'
+  $NOSETESTS -a 'yajl2,!slow'
 fi
