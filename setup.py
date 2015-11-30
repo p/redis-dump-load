6ac11b95a10d8078774d3832aefb47ca3829d787
Oleg Pudeyev <oleg@bsdpower.com>
2015-11-30 01:15:17 -0500

    Put data files in doc dir

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
+import os.path
 from distutils.core import setup
 
-setup(name='redis-dump-load',
-    version='0.4.0',
+package_name = 'redis-dump-load'
+package_version = '0.4.0'
+
+doc_dir = os.path.join('share', 'doc', package_name)
+
+data_files = ['LICENSE', 'README.rst']
+
+setup(name=package_name,
+    version=package_version,
     description='Dump and load redis databases',
     author='Oleg Pudeyev',
     author_email='oleg@bsdpower.com',
     url='http://github.com/p/redis-dump-load',
     py_modules=['redisdl'],
-    data_files=['LICENSE', 'README.rst'],
+    data_files=[
+        (doc_dir, data_files),
+    ],
 )
