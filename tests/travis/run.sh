42595a873ef57dedf79954fe25d12a70f68eff5f
Oleg Pudeyev <oleg@bsdpower.com>
2015-11-30 00:41:47 -0500

    Future travis setup for yajl2

--- tests/travis/run.sh
+++ tests/travis/run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+NOSETESTS = nosetests
+
 if test "$TEST" = slow; then
-  make test-slow
+  $(NOSETESTS) -a '!yajl2,slow'
 else
-  make test-quick
+  $(NOSETESTS) -a '!yajl2,!slow'
+fi
+
+if test -n "$YAJL2"; then
+  $(NOSETESTS) -a 'yajl2,!slow'
 fi
