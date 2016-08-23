f23f7b42f9ad98a0628c52301b1514d54f9fa9fa
Oleg Pudeyev <oleg@bsdpower.com>
2016-08-22 23:48:07 -0400

    more changes

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -3,3 +3,18 @@
 set -e
 
 pip install -r requirements-dev.txt
+
+case "$STREAMING_BACKEND" in
+  ijson)
+    pip install ijson;;
+  ijson-yajl)
+    pip install ijson yajl;;
+  ijson-yaql2)
+    pip install ijson yajl2;;
+  jsaone)
+    pip install jsaone;;
+  "")
+  *)
+    echo "Unknown streaming backend $STREAMING_BACKEND" 1>&2
+    exit 10;;
+esac
